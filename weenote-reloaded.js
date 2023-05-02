fd5f89418b95398ba56b52741d173569c485ba13
eyssette <cedric.eyssette@gmail.com>
2023-05-02 22:29:59 +0200

    Restructuration - externalisation fonctions+events

--- weenote-reloaded.js
+++ weenote-reloaded.js
@@ -1,4 +1,9 @@
 var start = false;
+var position = 1;
+var slide;
+var slides = {};
+
+// Event Listeners
 
 document.addEventListener("DOMContentLoaded", function () {
 	var startSlides = document.querySelectorAll(".startSlide");
@@ -11,51 +16,83 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 });
 
+const FORWARD = 1;
+const BACKWARD = -1;
+const keyToSlideDirection = {
+	ArrowRight: FORWARD,
+	ArrowLeft: BACKWARD,
+	PageDown: FORWARD,
+	PageUp: BACKWARD,
+};
+
+document.onkeydown = function (e) {
+	if (!start && e.key === "p") {
+		slideStart();
+	}
+	if (e.key === "Escape" || e.key === "Esc" || e.keyCode === 27) {
+		setTimeout(function () {
+			location.reload();
+		}, 100);
+	} else {
+		position = slide + keyToSlideDirection[e.key];
+		if (position in slides) location.hash = position;
+	}
+};
+
+document.ontouchstart = function (e) {
+	if (e.target.href) return;
+
+	var i = slide + (e.touches[0].pageX > innerWidth / 2 ? 1 : -1);
+
+	if (i in slides) location.hash = i;
+};
+
+// Fonction de calcul des styles pour chaque élément
+
+function calculateStyles(el) {
+	var style = el.style;
+	var i = 300; // Taille de police maximum
+	var top;
+	var left;
+
+	style.display = "inline";
+	style.fontSize = i + "px";
+	style.position = "absolute";
+
+	while (true) {
+		// Pour les paragraphes, la taille maximum de la police est 150
+		if (el.nodeName === "P" && i > 150) {
+			i = 150;
+		}
+		left = innerWidth - el.offsetWidth;
+		top = innerHeight - el.offsetHeight - 60; // Petit décalage vers le haut
+
+		if (top > 0 && left > 0) break;
+
+		style.fontSize = (i -= i * 0.05) + "px";
+	}
+
+	style.display = "block";
+	style.top = top / 2 + "px";
+	style.left = left / 2 + "px";
+
+	// Enregistre les styles calculés pour cet élément
+	el._styles = {
+		display: style.display,
+		fontSize: style.fontSize,
+		position: style.position,
+		top: style.top,
+		left: style.left,
+	};
+}
+
+// Démarrage des slides
+
 function slideStart() {
 	start = true;
 	var body = document.body;
 	var html = document.getElementsByTagName("html")[0];
 	html.classList.add("slides");
-	var slides = {};
-	var slide;
-
-	// Calcule les styles pour un élément donné
-	function calculateStyles(el) {
-		var style = el.style;
-		var i = 300; // Taille de police maximum
-		var top;
-		var left;
-
-		style.display = "inline";
-		style.fontSize = i + "px";
-		style.position = "absolute";
-
-		while (true) {
-			// Pour les paragraphes, la taille maximum de la police est 150
-			if (el.nodeName === "P" && i > 150) {
-				i = 150;
-			}
-			left = innerWidth - el.offsetWidth;
-			top = innerHeight - el.offsetHeight - 60; // Petit décalage vers le haut
-
-			if (top > 0 && left > 0) break;
-
-			style.fontSize = (i -= i * 0.05) + "px";
-		}
-
-		style.display = "block";
-		style.top = top / 2 + "px";
-		style.left = left / 2 + "px";
-
-		// Enregistre les styles calculés pour cet élément
-		el._styles = {
-			display: style.display,
-			fontSize: style.fontSize,
-			position: style.position,
-			top: style.top,
-			left: style.left,
-		};
-	}
 
 	for (var el, count = 0; (el = body.firstChild); ) {
 		if (el.nodeType == 1) {
@@ -68,15 +105,6 @@ function slideStart() {
 	body.appendChild(document.createComment(""));
 
 	!(function sync() {
-		document.onkeydown = function (e) {
-			if (e.key === "Escape" || e.key === "Esc" || e.keyCode === 27) {
-				location.reload();
-			} else {
-				var i = slide + keyToSlideDirection[e.key];
-				if (i in slides) location.hash = i;
-			}
-		};
-
 		setTimeout(sync, 10);
 
 		var next = 0 | location.hash.match(/\d+/);
@@ -96,27 +124,4 @@ function slideStart() {
 		next.style.top = styles.top;
 		next.style.left = styles.left;
 	})();
-
-	const FORWARD = 1;
-	const BACKWARD = -1;
-	const keyToSlideDirection = {
-		ArrowRight: FORWARD,
-		ArrowLeft: BACKWARD,
-		PageDown: FORWARD,
-		PageUp: BACKWARD,
-	};
-
-	document.ontouchstart = function (e) {
-		if (e.target.href) return;
-
-		var i = slide + (e.touches[0].pageX > innerWidth / 2 ? 1 : -1);
-
-		if (i in slides) location.hash = i;
-	};
 }
-
-document.onkeydown = function (e) {
-	if (!start && e.key === "p") {
-		slideStart();
-	}
-};
