e0ee2b893b293f879b7bb2e1a580d5f0a29c27a1
eyssette <cedric.eyssette@gmail.com>
2023-05-02 21:20:58 +0200

    Format document

--- weenote-reloaded.js
+++ weenote-reloaded.js
@@ -1,108 +1,111 @@
-var start = false
+var start = false;
 
 function slideStart() {
-  start = true;
-  var body = document.body;
-  var html = document.getElementsByTagName( 'html' )[0];
-html.classList.add("slides");
-  var slides = {};
-  var slide;
+	start = true;
+	var body = document.body;
+	var html = document.getElementsByTagName("html")[0];
+	html.classList.add("slides");
+	var slides = {};
+	var slide;
 
-  // Calcule les styles pour un élément donné
-  function calculateStyles(el) {
-    var style = el.style;
-    var i = 300; // Taille de police maximum
-    var top;
-    var left;
+	// Calcule les styles pour un élément donné
+	function calculateStyles(el) {
+		var style = el.style;
+		var i = 300; // Taille de police maximum
+		var top;
+		var left;
 
-    style.display = "inline";
-    style.fontSize = i + "px";
-    style.position = "absolute";
+		style.display = "inline";
+		style.fontSize = i + "px";
+		style.position = "absolute";
 
-    while (1) {
-      // Pour les paragraphes, la taille maximum de la police est 150
-      if (el.nodeName =="P" && i>150) {
-        i=150
-      }
-      left = innerWidth - el.offsetWidth;
-      top = innerHeight - el.offsetHeight;
+		while (1) {
+			// Pour les paragraphes, la taille maximum de la police est 150
+			if (el.nodeName == "P" && i > 150) {
+				i = 150;
+			}
+			left = innerWidth - el.offsetWidth;
+			top = innerHeight - el.offsetHeight;
 
-      if (top > 0 && left > 0) break;
+			if (top > 0 && left > 0) break;
 
-      style.fontSize = (i -= i * 0.05) + "px";
-    }
+			style.fontSize = (i -= i * 0.05) + "px";
+		}
 
-    style.display = "block";
-    style.top = top / 2 + "px";
-    style.left = left / 2 + "px";
-    
-    // Enregistre les styles calculés pour cet élément
-    el._styles = {
-      display: style.display,
-      fontSize: style.fontSize,
-      position: style.position,
-      top: style.top,
-      left: style.left
-    };
-  }
+		style.display = "block";
+		style.top = top / 2 + "px";
+		style.left = left / 2 + "px";
 
-  for (var el, count = 0; el = body.firstChild;) {
-    if (el.nodeType == 1) {
-      calculateStyles(el);
-      slides[++count] = el;
-    }
-    body.removeChild(el);
-  }
+		// Enregistre les styles calculés pour cet élément
+		el._styles = {
+			display: style.display,
+			fontSize: style.fontSize,
+			position: style.position,
+			top: style.top,
+			left: style.left,
+		};
+	}
 
-  body.appendChild(document.createComment(""));
+	for (var el, count = 0; (el = body.firstChild); ) {
+		if (el.nodeType == 1) {
+			calculateStyles(el);
+			slides[++count] = el;
+		}
+		body.removeChild(el);
+	}
 
-  !function sync() {
-    document.onkeydown = function(e) {
-      if (e.key === "Escape" || e.key === "Esc" || e.keyCode === 27) {location.reload();}
-      else {
-        var i = slide + keyToSlideDirection[e.key]
-        if (i in slides) location.hash = i
-      }
-    }
+	body.appendChild(document.createComment(""));
 
-    setTimeout(sync, 10);
+	!(function sync() {
+		document.onkeydown = function (e) {
+			if (e.key === "Escape" || e.key === "Esc" || e.keyCode === 27) {
+				location.reload();
+			} else {
+				var i = slide + keyToSlideDirection[e.key];
+				if (i in slides) location.hash = i;
+			}
+		};
 
-    var next = 0 | location.hash.match(/\d+/);
+		setTimeout(sync, 10);
 
-    if (slide == next) return;
+		var next = 0 | location.hash.match(/\d+/);
 
-    next = Math.max(Math.min(count, next), 1);
-    next = slides[location.hash = slide = next];
+		if (slide == next) return;
 
-    body.replaceChild(next, body.firstChild);
+		next = Math.max(Math.min(count, next), 1);
+		next = slides[(location.hash = slide = next)];
 
-    // Applique les styles enregistrés pour cet élément
-    var styles = next._styles;
-    next.style.display = styles.display;
-    next.style.fontSize = styles.fontSize;
-    next.style.position = styles.position;
-    next.style.top = styles.top;
-    next.style.left = styles.left;
-  }();
+		body.replaceChild(next, body.firstChild);
 
-  const FORWARD = 1;
-  const BACKWARD = -1;
-  const keyToSlideDirection = {
-    "ArrowRight": FORWARD,
-    "ArrowLeft": BACKWARD,
-    "PageDown": FORWARD,
-    "PageUp": BACKWARD
-  };
+		// Applique les styles enregistrés pour cet élément
+		var styles = next._styles;
+		next.style.display = styles.display;
+		next.style.fontSize = styles.fontSize;
+		next.style.position = styles.position;
+		next.style.top = styles.top;
+		next.style.left = styles.left;
+	})();
 
-  document.ontouchstart = function(e) {
-    if (e.target.href) return;
+	const FORWARD = 1;
+	const BACKWARD = -1;
+	const keyToSlideDirection = {
+		ArrowRight: FORWARD,
+		ArrowLeft: BACKWARD,
+		PageDown: FORWARD,
+		PageUp: BACKWARD,
+	};
 
-    var i = slide + (e.touches[0].pageX > innerWidth / 2 ? 1 : -1);
+	document.ontouchstart = function (e) {
+		if (e.target.href) return;
 
-    if (i in slides) location.hash = i;
-  };
-};
+		var i = slide + (e.touches[0].pageX > innerWidth / 2 ? 1 : -1);
 
-document.onkeydown = function(e) {
-  if (!start && e.key==="p") {slideStart()}
+		if (i in slides) location.hash = i;
+	};
 }
+
+document.onkeydown = function (e) {
+	if (!start && e.key === "p") {
+		slideStart();
+	}
+};
