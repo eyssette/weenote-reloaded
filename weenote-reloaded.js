2a29613afb25a9763f562448cd0fb8f25f84d91d
eyssette <cedric.eyssette@gmail.com>
2023-05-03 15:58:38 +0200

    Recherche police optimale ssi il y a du texte

--- weenote-reloaded.js
+++ weenote-reloaded.js
@@ -66,6 +66,7 @@ function calculateStyles(el) {
 	style.position = "absolute";
 
 	while (true) {
+		if (!el.textContent) break // S'il n'y a pas de contenu texte, il ne faut pas chercher la taille de police optimale !
 		// Pour les paragraphes, la taille maximum de la police est 150
 		if (el.nodeName === "P" && i > 150) {
 			i = 150;
@@ -73,7 +74,7 @@ function calculateStyles(el) {
 		left = innerWidth - el.offsetWidth;
 		top = innerHeight - el.offsetHeight - 60; // Petit décalage vers le haut
 
-		if ((top > 0 && left > 0) || i < 10) break; // Taille de police minimum : 10. Important de fixer une taille minimum en cas de bloc sans texte (p.ex. : image)
+		if ((top > 0 && left > 0) || i < 10) break; // Taille de police minimum : 10
 
 		style.fontSize = (i -= i * 0.05) + "px";
 	}
